b862560ba2ea4f74cccd38d3a8b9d1c8183e55e8
Subrata Dey <s.dey@devon.nl>
2023-04-17 12:10:27 +0530

    Added WEb application

---
+++ ExactOpenAIDemo/Model/ReadFile.cs
@@ -0,0 +1,12 @@
+﻿namespace ExactOpenAIDemo.Model
+{
+    public class ReadFile
+    {
+        public string ReadContentOfTheFile()
+        {
+            string filePath = "C:\\JsonFile\\Person.json";
+            string jsonText = File.ReadAllText(filePath);
+            return jsonText;
+        }
+    }
+}
